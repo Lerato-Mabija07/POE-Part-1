ddd761754e71b90d83c4cadc5ddcd767f7a9c641
lab_services_student <lab_services_student@labVMH8OX>
2024-04-12 17:24:14 +0200

    Add project files.

---
+++ POE Part 1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Recipe_Application
+{
+    class Ingredient
+    {
+        public string Name { get; set; }
+        public int NumIngredients { get; set; }
+        public string Description { get; set; }
+    }
+}
