47e919e005b7348a0b7ac000f091747d384d74dd
Lerato-Mabija07 <ST10365633@imconnect.edu.za>
2024-04-12 17:55:14 +0200

    User's prompted to add input. If user enters a non-numeric value, an error message will be displayed the loop will restart using "continue".

--- POE Part 1/Program.cs
+++ POE Part 1/Program.cs
@@ -9,10 +9,17 @@ namespace RecipeApp
             Recipe recipe = new Recipe();
             while (true)
             {
-                Console.WriteLine("1. Enter recipe details");
-                Console.WriteLine("2. Display recipe");
-                Console.WriteLine("3. Scale recipe");
-                Console.WriteLine("4. Reset quantities");
-                Console.WriteLine("5. Clear all data");
-                Console.WriteLine("6. Exit");
+                Console.WriteLine("1. Enter the details of the recipe");
+                Console.WriteLine("2. Display the recipe");
+                Console.WriteLine("3. Scale the recipe");
+                Console.WriteLine("4. Reset the quantities");
+                Console.WriteLine("5. Clear all the data");
+                Console.WriteLine("6. Exit the Application");
+                int choice;
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Invalid input. Please enter a number between 1 and 6.");
+                    continue;
+                }
+
 
